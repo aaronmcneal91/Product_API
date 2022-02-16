5065e5296d61a7e20b493a6f6b28f983b35e2f73
unknown <your email>
2022-02-16 15:46:50 -0500

    Completed GET POST PUT DELETE

--- products/views.py
+++ products/views.py
@@ -1,3 +1,6 @@
+from ast import Delete
+from operator import ge
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -20,6 +23,32 @@ def product_list(request):
         serializer.is_valid(raise_exception= True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status.HTTP_204_NO_CONTENT)
+        
+
+  
+
+
+
+
+    
+        products = Productserializer.data
     
 
 
